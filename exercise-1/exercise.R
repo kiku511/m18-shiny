7bb11ee9fbffb2f6e8add524ada10aef1b7c5cb4
Vansh <vansh511@uw.edu>
2017-05-20 18:59:58 -0700

    completes 18.1, learned adding scripts

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,10 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/Desktop/info_201/m18-shiny/exercise-1")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("./scripts/BuildScatter.R")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+BuildScatter(iris, "Petal.Length", "Petal.Width", "Species")
